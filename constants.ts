dc6771bb31d21e1b49996326e5452d116c3e3297
ryan1260507-lang <ryan1260507@monogemini-v2.com>
2025-12-11 02:25:36 +0900

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,18 +1,18 @@
 // Images
-export const BG_IMAGE = './santa_village_background.png';
-export const SOCK_IMAGE = './socks.png';
+// 주의: 아래 파일들은 프로젝트 루트의 'public' 폴더 안에 위치해야 합니다.
+export const BG_IMAGE = 'santa_village_background.png';
+export const SOCK_IMAGE = 'socks.png';
 
 // Generate paths for gifts 1 to 35
-export const getGiftImage = (id: number) => `./${id}.png`;
+export const getGiftImage = (id: number) => `${id}.png`;
 
 // Configuration
 export const TOTAL_SOCKS = 35;
 export const DRUMROLL_DURATION = 3000; // ms
 
 // Sound Resources
-// Reverted to local paths as requested
 export const SOUNDS = {
-  DRUMROLL: './drum.mp3', 
-  TADA: './ding.mp3',
-  BGM: './bgm.mp3'
+  DRUMROLL: 'drum.mp3', 
+  TADA: 'ding.mp3',
+  BGM: 'bgm.mp3'
 };
